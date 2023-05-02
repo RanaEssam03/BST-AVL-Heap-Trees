e72760e47f082694e4be80a2ba633b27f26ffb9c
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2023-05-02 19:17:10 +0300

    Update Student.h

---
+++ Student.h
@@ -0,0 +1,52 @@
+//
+// Created by Rana Essam on 5/2/2023.
+//
+
+#ifndef BST_AVL_HEAP_TREES_STUDENT_H
+#define BST_AVL_HEAP_TREES_STUDENT_H
+#include <iostream>
+#include <utility>
+
+using namespace std;
+struct Student {
+    int id{};
+    string name;
+    float gpa{};
+    string dep;
+    Student(string name, int id, float gpa, string dep){
+        this->gpa = gpa;
+        this->name = name;
+        this->id = id;
+        this->dep = dep;
+    }
+    Student()= default;
+
+    friend bool operator > (const Student& s1, const Student& s2)
+    {
+        return s1.gpa > s2.gpa;
+    }
+
+    friend bool operator < (const Student& s1, const Student& s2)
+    {
+        return s1.gpa < s2.gpa;
+    }
+
+    friend bool operator >= (const Student& s1, const Student& s2)
+    {
+        return s1.gpa >= s2.gpa;
+    }
+
+    friend bool operator <= (const Student& s1, const Student& s2)
+    {
+        return s1.gpa > s2.gpa;
+    }
+
+    friend bool operator == (const Student& s1, const Student& s2)
+    {
+        return s1.gpa > s2.gpa;
+    }
+
+};
+
+
+#endif //BST_AVL_HEAP_TREES_STUDENT_H
